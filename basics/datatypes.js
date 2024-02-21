24c6588bd88bf79ae63909df6cf6f214fa102de4
Aman kumar <133964541+Aman-Kumar342@users.noreply.github.com>
2024-02-21 12:56:32 +0000

    datatypes and variable

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)//we are using nodejs, not browser it will throw error 
+// code readiablity should be high 
+// read documentry from tc39 and mdn
+
+console.log(3+3)
+console.log("hitesh")
+
+let name="hitesh"
+let age =18
+let isloggedIn=false
+let state;//null which is a object data type 
+//console.log(typeof null) => object
+
+//study on ecmascript
+
+//symbol for unique
+//object
+//undefined
+//typeof
+
+console.log(typeof undefined);
+
---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId=14453
+let accountEmail="amantop102525@gmail.com"
+var accountPassword="12345"
+accountCity="chennai"
+let accountstates;
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+/*
+prefer not to use var beacuse of issue 
+in block scope and functional scope
+*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstates])
