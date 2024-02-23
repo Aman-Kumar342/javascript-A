fa903ba58df9e638d95fafc29fa3d2ba118f0dc0
Aman kumar <133964541+Aman-Kumar342@users.noreply.github.com>
2024-02-23 16:48:54 +0000

    TEST

---
+++ basics/conversionsandoperations.js
@@ -0,0 +1,26 @@
+let score=33
+
+console.log(typeof score);
+
+let score1="33"
+let valueIntNumber=Number(score1)
+console.log(typeof valueIntNumber);
+
+let score2="33abc"
+let valueIntNumber1=Number(score2)
+console.log(typeof valueIntNumber1)
+
+//33abc is NaN i.e not a number
+// for undefinred also it throw NaN
+
+// if score =null then 
+//let score1="33"
+//let valueIntNumber=Number(score1)
+//console.log(typeof valueIntNumber); =>0
+
+
+let isloggedin="Aman"
+let booleanisloggedin=Boolean(isloggedin)
+// 1-> true 0->false
+// ""->false "aman"->true
+
